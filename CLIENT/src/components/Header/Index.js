6189e6b6c82f10f7eae9bdf976ce75a22dbf3e27
JiaJie Chen <jiajiechen@Jay-MacBook-Pro.local>
2021-10-02 19:22:02 -0700

    files organized

---
+++ CLIENT/src/components/Header/Index.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import './index.css';
+import { QUERY_ME } from "../../utils/queries"
+import { useQuery } from "@apollo/react-hooks"
+import { useStoreContext } from "../../utils/GlobalState"
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+ const { loading, error, data } = useQuery(QUERY_ME)
+ const { globalStore, dispatch } = useStoreContext()
+
+ useEffect(function () {
+   if (data !== undefined) {
+     dispatch({
+       type: "LOGIN",
+       payload: data.me
+     })
+   }
+ }, [data]);
+
+ console.log(data);
+
+ return (
+   <div className="HeaderContainer">
+     {loading ? <p></p>: <p></p>}
+     <Link to="/" className="HeaderLogo"/>
+   </div>
+ );
+}
+
+export default Header;
